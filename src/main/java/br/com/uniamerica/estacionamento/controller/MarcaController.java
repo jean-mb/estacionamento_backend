251ffdf8160e88e13aff8a386720fb826ec99acb
Jean Moschen - Home <jeanfelipe0610@gmail.com>
2023-04-27 20:37:48 -0300

    Controller completo para Marca

--- src/main/java/br/com/uniamerica/estacionamento/controller/MarcaController.java
+++ src/main/java/br/com/uniamerica/estacionamento/controller/MarcaController.java
@@ -5,15 +5,28 @@ import br.com.uniamerica.estacionamento.repository.MarcaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping(value = "/api/marca")
 public class MarcaController {
     @Autowired
     private MarcaRepository marcaRepository;
+
+    @GetMapping
+    public ResponseEntity<?> findById(@RequestParam("id") final Long id){
+        final Marca marca =  this.marcaRepository.findById(id).orElse(null);
+        return marca == null ? ResponseEntity.badRequest().body("Nenhum modelo encontrado") : ResponseEntity.ok(marca);
+    }
+
+    @GetMapping("/lista")
+    public ResponseEntity<?> listarAll(){
+        return ResponseEntity.ok(this.marcaRepository.findAll());
+    }
+    @GetMapping("/lista/ativos")
+    public ResponseEntity<?> listarAtivos(){
+        return ResponseEntity.ok(this.marcaRepository.findAllAtivo());
+    }
 
     @PostMapping
     public ResponseEntity<?> cadastraMarca(@RequestBody final Marca marca){
@@ -22,6 +35,40 @@ public class MarcaController {
             return ResponseEntity.ok("Marca cadastrada com sucesso");
         }catch (Exception e){
             return ResponseEntity.badRequest().body(e.getCause().getCause().getMessage());
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<?> atualizarMarca(
+            @RequestParam("id") final Long id,
+            @RequestBody Marca marca
+    ){
+        try {
+
+            final Marca marcaBanco = this.marcaRepository.findById(id).orElse(null);
+
+            if (marcaBanco == null || !marcaBanco.getId().equals(marca.getId())) {
+                throw new RuntimeException("Não foi possivel identifica a marca informada");
+            }
+            this.marcaRepository.save(marca);
+            return ResponseEntity.ok("Marca atualizada com sucesso");
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deletar(
+            @RequestParam("id") final Long id
+    ){
+        try{
+            final Marca marcaBanco = this.marcaRepository.findById(id).orElse(null);
+            assert marcaBanco != null;
+            this.marcaRepository.delete(marcaBanco);
+            return ResponseEntity.ok("Marca atualizada com sucesso");
+
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 }
--- src/main/java/br/com/uniamerica/estacionamento/repository/MarcaRepository.java
+++ src/main/java/br/com/uniamerica/estacionamento/repository/MarcaRepository.java
@@ -8,9 +8,9 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface MarcaRepository extends JpaRepository<Marca, Long> {
-    public List<Marca> findByNome(final String nome);
     @Query("from Marca where nome like :nome")
     public List<Marca> findByNomeLike(@Param("nome") final String nome);
-    @Query(value = "select * from marcas where nome like :nome", nativeQuery = true)
-    public List<Marca> findByNomeLikeNative(@Param("nome") final String nome);
+    @Query("from Marca where ativo = true")
+    public List<Marca> findAllAtivo();
+
 }
