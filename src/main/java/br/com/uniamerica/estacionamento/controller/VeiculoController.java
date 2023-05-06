7374472dfc880a8c8e761ad2dd4d75301640bbbd
Jean Moschen - Home <jeanfelipe0610@gmail.com>
2023-05-05 22:54:20 -0300

    VeiculoService.java concluido

--- src/main/java/br/com/uniamerica/estacionamento/controller/VeiculoController.java
+++ src/main/java/br/com/uniamerica/estacionamento/controller/VeiculoController.java
@@ -1,7 +1,6 @@
 package br.com.uniamerica.estacionamento.controller;
 
 import br.com.uniamerica.estacionamento.entity.Veiculo;
-import br.com.uniamerica.estacionamento.repository.MovimentacaoRepository;
 import br.com.uniamerica.estacionamento.repository.VeiculoRepository;
 import br.com.uniamerica.estacionamento.service.VeiculoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +13,6 @@ import org.springframework.web.bind.annotation.*;
 public class VeiculoController {
     @Autowired
     private VeiculoRepository veiculoRepository;
-
-    @Autowired
-    private MovimentacaoRepository movimentacaoRepository;
 
     @Autowired
     private VeiculoService veiculoService;
@@ -50,13 +46,9 @@ public class VeiculoController {
             @RequestParam("id") final Long id,
             @RequestBody final Veiculo veiculo
     ){
-        try{
-            final Veiculo veiculoBanco = this.veiculoRepository.findById(id).orElse(null);
-            if(veiculoBanco == null || !veiculoBanco.getId().equals(veiculo.getId())){
-                throw new RuntimeException("Nenhum veiculo identificado");
-            }
-            this.veiculoRepository.save(veiculo);
-            return ResponseEntity.ok("Veiculo atualizado com sucesso!");
+        try {
+            final Veiculo newVeiculo = this.veiculoService.editar(id, veiculo);
+            return ResponseEntity.ok(String.format("Veiulo placa [ %s ] editado com sucesso!", newVeiculo.getPlaca()));
         }catch (Exception e){
             return ResponseEntity.badRequest().body(e.getMessage());
         }
@@ -67,18 +59,7 @@ public class VeiculoController {
             @RequestParam("id") final Long id
     ){
         try{
-            final Veiculo veiculoBanco = this.veiculoRepository.findById(id).orElse(null);
-            if(veiculoBanco == null){
-                throw new RuntimeException("Veiculo não encontrado");
-            }
-            if(!this.movimentacaoRepository.findByVeiculoId(id).isEmpty()){
-                veiculoBanco.setAtivo(false);
-                this.veiculoRepository.save(veiculoBanco);
-                return ResponseEntity.ok("Veiculo desativado com sucesso!");
-            }else{
-                this.veiculoRepository.delete(veiculoBanco);
-                return ResponseEntity.ok("Veiculo apagado com sucesso!");
-            }
+            return this.veiculoService.desativar(id);
         }catch (Exception e){
             return ResponseEntity.badRequest().body(e.getMessage());
         }
--- src/main/java/br/com/uniamerica/estacionamento/service/VeiculoService.java
+++ src/main/java/br/com/uniamerica/estacionamento/service/VeiculoService.java
@@ -3,8 +3,10 @@ package br.com.uniamerica.estacionamento.service;
 import br.com.uniamerica.estacionamento.entity.Modelo;
 import br.com.uniamerica.estacionamento.entity.Veiculo;
 import br.com.uniamerica.estacionamento.repository.ModeloRepository;
+import br.com.uniamerica.estacionamento.repository.MovimentacaoRepository;
 import br.com.uniamerica.estacionamento.repository.VeiculoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
@@ -17,6 +19,8 @@ public class VeiculoService {
     private VeiculoRepository veiculoRepository;
     @Autowired
     private ModeloRepository modeloRepository;
+    @Autowired
+    private MovimentacaoRepository movimentacaoRepository;
 
     @Transactional
     public Veiculo cadastrar(Veiculo veiculo){
@@ -34,5 +38,59 @@ public class VeiculoService {
         Assert.notNull(modelo, "Modelo informado não existe!");
 
         return this.veiculoRepository.save(veiculo);
+    }
+    @Transactional
+    public Veiculo editar(Long id, Veiculo veiculo){
+        /*
+         * Verifica se o veiculo existe
+         */
+        final Veiculo veiculoBanco = this.veiculoRepository.findById(id).orElse(null);
+        Assert.notNull(veiculoBanco, "Veiculo não existe!");
+
+        /*
+         * Verifica os veiculos coincidem
+         */
+        Assert.isTrue(veiculoBanco.getId().equals(veiculo.getId()), "Veiculo informado não é o mesmo que o veiculo a ser atualizado");
+
+        /*
+         * Verifica os campos que são notNull
+         * */
+        Assert.notNull(veiculo.getCadastro(), "Data do cadastro não informada!");
+        Assert.notNull(veiculo.getPlaca(), "Placa do veiculo não informada!");
+        Assert.hasText(veiculo.getPlaca(), "Placa do veiculo vazia!");
+        Assert.notNull(veiculo.getCor(), "Cor do veiculo não informada!");
+        Assert.notNull(veiculo.getTipo(), "Tipo do veiculo não informado!");
+
+        /*
+         * Verifica se modelo existe
+         * */
+        Assert.notNull(veiculo.getModelo(), "Modelo não informado!");
+        final Modelo modelo = this.modeloRepository.findById(veiculo.getModelo().getId()).orElse(null);
+        Assert.notNull(modelo, "Modelo não existe!");
+
+        return this.veiculoRepository.save(veiculo);
+    }
+    @Transactional
+    public ResponseEntity<?> desativar(Long id){
+
+        /*
+         * Verifica se o Veiculo informado existe
+         * */
+        final Veiculo veiculoBanco = this.veiculoRepository.findById(id).orElse(null);
+        Assert.notNull(veiculoBanco, "Modelo não encontrado!");
+
+        /*
+         * Verifica se o Veiculo informado está relacionado a um Veiculo,
+         * True: Desativa o cadastro
+         * False: Faz o DELETE do registro
+         * */
+        if(!this.movimentacaoRepository.findByVeiculoId(id).isEmpty()){
+            veiculoBanco.setAtivo(false);
+            this.veiculoRepository.save(veiculoBanco);
+            return ResponseEntity.ok( String.format("Veiculo com placa [ %s ] DESATIVADO pois está relacionado a movimentações!", veiculoBanco.getPlaca()));
+        }else{
+            this.veiculoRepository.delete(veiculoBanco);
+            return ResponseEntity.ok(String.format("Veiculo com placa [ %s ] DELETADO com sucesso!", veiculoBanco.getPlaca()));
+        }
     }
 }
