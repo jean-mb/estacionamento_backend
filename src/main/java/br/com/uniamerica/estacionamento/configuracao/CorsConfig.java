e5e85c3efcdb9d74c8ef12bb7071f3bdce0d6466
Jean Moschen - Pessoal <jeanfelipe0610@gmail.com>
2023-06-22 19:40:07 -0300

    Cors config

---
+++ src/main/java/br/com/uniamerica/estacionamento/configuracao/CorsConfig.java
@@ -0,0 +1,17 @@
+package br.com.uniamerica.estacionamento.configuracao;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Content-Type")
+                .allowCredentials(true);
+    }
+}
