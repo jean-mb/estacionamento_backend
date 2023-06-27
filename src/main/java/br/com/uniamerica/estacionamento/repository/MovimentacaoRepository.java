7dac22e82aa48d5894d15935ec55f29e3fc45d78
Jean - Autofoz <jeanfelipe0610@gmail.com>
2023-06-27 09:14:13 -0300

    code cleanup

--- src/main/java/br/com/uniamerica/estacionamento/repository/MovimentacaoRepository.java
+++ src/main/java/br/com/uniamerica/estacionamento/repository/MovimentacaoRepository.java
@@ -8,11 +8,11 @@ import java.util.List;
 
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Long> {
     @Query("from Movimentacao where condutor.id = :id")
-    public List<Movimentacao> findByCondutorId(@Param("id") final Long id);
+    List<Movimentacao> findByCondutorId(@Param("id") final Long id);
     @Query("from Movimentacao where dataSaida = null and ativo = true")
-    public List<Movimentacao> findAllAbertas();
+    List<Movimentacao> findAllAbertas();
     @Query("from Movimentacao where veiculo.id = :id")
-    public List<Movimentacao> findByVeiculoId(@Param("id") final Long id);
+    List<Movimentacao> findByVeiculoId(@Param("id") final Long id);
     @Query("from Movimentacao where veiculo.id = :id and dataSaida = null and ativo = true")
-    public List<Movimentacao> getEstacionado(@Param("id") final Long id);
+    List<Movimentacao> getEstacionado(@Param("id") final Long id);
 }
